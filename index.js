51bc87b6d31b08eb67661b04e4acbce068d0c40f
ptawuringa <ptaruwinga@yahoo.com>
2020-11-17 09:53:34 +0200

    init

--- index.js
+++ index.js
@@ -5,39 +5,81 @@ let express = require('express');
 const numberPlates = require('./registration_number')
 let app = express();
 
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
 
- // initialise session middleware - flash-express depends on it
- app.use(session({
-    secret : "<This is my string that i used for session>",
-    resave: false,
-    saveUninitialized: true
-  }));
+const pg = require("pg");
+const Pool = pg.Pool;
 
-  // initialise the flash middleware
-  app.use(flash());
+const connectionString = process.env.DATABASE_URL || 'postgresql://taruwinga:numbers1@localhost:5432/reg_numbers';
 
-  app.engine('handlebars', exphbs({ layoutsDir: "./views/layouts/"}));
-  app.set('view engine', 'handlebars');
+const pool = new Pool({
+  connectionString,
+
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+const regX = numberPlates(pool)
+
+// initialise session middleware - flash-express depends on it
+app.use(session({
+  secret: "<This is my string that i used for session>",
+  resave: false,
+  saveUninitialized: true
+}));
+
+// initialise the flash middleware
+app.use(flash());
+
+app.engine('handlebars', exphbs({ layoutsDir: "./views/layouts/" }));
+app.set('view engine', 'handlebars');
 
 
-app.get("/", function(req, res){
-    res.render("index");
-  });
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 app.get('/addFlash', function (req, res) {
-    req.flash('info', 'Flash Message Added');
-    res.redirect('/');
-  });
+  req.flash('info', 'Flash Message Added');
+  res.redirect('/');
+});
 
 app.use(express.static('public'));
 
-app.post('/registration_number',numberPlates);
+app.get('/registration/:regType', function (req, res) {
+  
+
+  res.render('reg', {
+    regType,
+    reg
+
+  })
+
+});
+
+
+app.post('/regNumbers', async function (req, res) {
+
+  var reg = req.body.regNumber
+  await regX.addPlate(reg)
+
+
+  // console.log(reg)
 
 
 
-let PORT = process.env.PORT || 3014;
 
-app.listen(PORT, function(){
-  console.log('App starting on port', PORT);
+
+  res.render("index");
+});
+
+
+
+let PORT = process.env.PORT || 3004;
+
+app.listen(PORT, function () {
+  console.log('App starting on port', PORT)
 });
--- registration_number.js
+++ registration_number.js
@@ -1,90 +1,23 @@
-module.exports = function regNumbers(registrations){
+module.exports = function regNumbers(pool){
 
-    function isFromBellville(vehicle){
-        if(vehicle = vehicle. startsWith("CA")){
-              return true;
-        }
-        return false;
-      }
+   async function addPlate(reg){
+    var num = reg.substring(0.2);
+    let motor = await pool.query('SELECT id FROM registration_numbers WHERE reg_number = $1',[reg]);
+    console.log(motor)
 
-      function isFromPaarl(vehicle){
-        if(vehicle = vehicle.startsWith("CJ")){
-          return true;
-        }
-        return false;
-      }
+   }
 
-      function isFromCapeTown(vehicle){
-        if(vehicle = vehicle.startsWith("CY")){
-          return true;
-        }
-        return false;
-      }
-
-      function isFrom(regNum,location){
-  
-        if(regNum.startsWith(location)){
-          return true;
-      }
-      return false;
-        
-      }
-
-      function regCheck(regPlates,location){
-        var confirm = regPlates.endsWith(location);
-               return confirm;
-         }
-
-
-const regNumbersEntered = [];
-
-addRegNumberBtn.addEventListener("click", function() {
-
-	if (regNumberEntered.value == "") {
-		req.flash  ("Please enter a registration number");
-		return;
-	}
-
-	const currentRegnumber = regNumberEntered.value.toUpperCase();
-
-	if (currentRegnumber.startsWith ("CY") ||
-		currentRegnumber.startsWith ("CA") ||
-		currentRegnumber.startsWith ("CJ")) {
-
-	    message.innerHTML = "";
-
-	if (!regNumbersEntered.includes(currentRegnumber)){
-
-	
-	// add these 3 lines
-
- const regNumberElem = document.createElement("div");
- regNumberElem.classList.add("regNumber");
- regNumberElem.innerHTML = currentRegnumber;
- regNumbers.appendChild(regNumberElem);
-
- regNumbersEntered.push(currentRegnumber)
-
-
-
-	} else {
-		req.flash  ("This registration number already exists.");
-	}
-}else{
-	req.flash ("Invalid registration number entered");
-}
-
-});
+    
+         
 
 
       return {
-          isFromBellville,
-          isFromCapeTown,
-          isFromPaarl,
-          isFrom,
-          regCheck
+            addPlate,
+          
 
-      }
+       }
 
 
-}
+  
+
+  }
